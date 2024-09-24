af49c34c7839425a65a4e59ee0f7aec64447080c
Cristopher Matias <pca_1997_10@hotmail.com>
2024-09-23 20:49:11 -0500

    carga final

--- src/main/java/pe/edu/cibertec/patitas_backend_a/PatitasBackendAApplication.java
+++ src/main/java/pe/edu/cibertec/patitas_backend_a/PatitasBackendAApplication.java
@@ -2,10 +2,6 @@ package pe.edu.cibertec.patitas_backend_a;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @SpringBootApplication
@@ -14,19 +10,6 @@ public class PatitasBackendAApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PatitasBackendAApplication.class, args);
 	}
-	@Bean
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
+
 	}
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-						.allowedOrigins("http://localhost:8080")  // Cambia por el puerto del frontend
-						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
-			}
-		};
-	}
-}
+
